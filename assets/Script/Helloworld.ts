35b7edd4baae0c19b0ddd86180da31d7fcba63a6
Christian Osuna <sakurai@christismacbook.lan>
2020-01-24 15:03:21 -0800

    made porter move to cursor

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -3,14 +3,21 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class Helloworld extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
+    @property(cc.Node)
+    player: cc.Node = null;
 
     start () {
         // init logic
-        this.label.string = this.text;
+        
+        this.node.on(cc.Node.EventType.MOUSE_DOWN, (event: cc.Event.EventMouse) => {
+            
+            if (this.player) {
+                const move = cc.moveTo (1, event.getLocation())
+                
+                this.player.runAction(move)
+            }
+            cc.log(event.getLocation())
+            cc.log(this.node.getAnchorPoint())
+        })
     }
 }
