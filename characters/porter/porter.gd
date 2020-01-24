6961bfe5a21ce4ff1f38cd714869c29bb47aa966
Michael Osuna Jr <mikeosunajr@gmail.com>
2020-01-23 23:03:47 -0800

    add default splashes for ios and make porter walk around obstacles

--- characters/porter/porter.gd
+++ characters/porter/porter.gd
@@ -2,41 +2,44 @@ extends KinematicBody2D
 
 export (int) var speed = 400
 
-var target = Vector2()
-var velocity = Vector2()
-var up_right = Vector2(1, 1).normalized()
-
-func _input(event):
-	if event.is_action_pressed('click'):
-		target = get_global_mouse_position()
-
-func default():
-	$Sprite.play(add_direction("stand"))
+export var path = PoolVector2Array()
+var lastDirection = "%s_d"
 
 func add_direction(anim):
-	if abs(velocity.x) > abs(velocity.y):
-		if velocity.x > 0:
-			return "%s_r" % anim
+	if path.size() == 0:
+		return lastDirection % anim
+		
+	var direction = path[0] - position
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			lastDirection = "%s_r"
 		else:
-			return "%s_l" % anim
-	elif velocity.y > 0:
-		return "%s_d" % anim
+			lastDirection = "%s_l"
+	elif direction.y > 0:
+		lastDirection = "%s_d"
 	else:
-		return "%s_u" % anim
+		lastDirection = "%s_u"
+	return lastDirection % anim
 
-func walk():
-	var collide = move_and_collide(velocity)
-	if collide:
-		default()
-		target = position
-		velocity = Vector2.ZERO
-	else:
+func _process(delta):
+	# Calculate the movement distance for this frame
+	var distance_to_walk = speed * delta
+	
+	# Move the player along the path until he has run out of movement or the path ends.
+	while distance_to_walk > 0 and path.size() > 0:
+		var distance_to_next_point = position.distance_to(path[0])
+		if distance_to_walk <= distance_to_next_point:
+			# The player does not have enough movement left to get to the next point.
+			position += position.direction_to(path[0]) * distance_to_walk
+		else:
+			# The player get to the next point
+			position = path[0]
+			path.remove(0)
+		# Update the distance to walk
+		distance_to_walk -= distance_to_next_point
+		
+	if path.size() > 0:
 		$Sprite.play(add_direction("walk"))
-
-func _physics_process(delta):
-	velocity = (target - position).normalized() * speed * delta
-	# rotation = velocity.angle()
-	if (target - position).length() > 5:
-		walk()
 	else:
-		default()
+		$Sprite.play(add_direction("stand"))
+	
--- main.gd
+++ main.gd
@@ -11,3 +11,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _input(event):
+	if event.is_action_pressed('click'):
+		var path = $Navigation2D.get_simple_path($Player.position, get_global_mouse_position())
+		$Line2D.points = path
+		$Player.path = path
